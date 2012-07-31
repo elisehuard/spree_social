b22dbf7b50167316bb4aef1abcb4835f1343b688
Elise Huard <github@elisehuard.be>
2012-07-31 12:16:08 +0100

    Revert "redirection to root instead of user account"
    This reverts commit 00f6fec617013513a0e438f0a7ff6e56121b0a66.

--- app/controllers/spree/user_registrations_controller_decorator.rb
+++ app/controllers/spree/user_registrations_controller_decorator.rb
@@ -11,7 +11,7 @@ Spree::UserRegistrationsController.class_eval do
     if session[:omniauth]
       @user.apply_omniauth(session[:omniauth])
       @user.valid?
-      redirect_to "/"
+      @user
     end
   end
 end
